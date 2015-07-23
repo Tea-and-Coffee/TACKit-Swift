f165672837b1d8894b720217328c9449a2dc641d
companion_ios <companion_ios@Satoru-no-iMac.local>
2015-07-23 11:41:45 +0900

    Create TACKitSwiftExample.

---
+++ TACKitSwiftExample/TACKitSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TACKitSwiftExample
+//
+//  Created by companion_ios on 20150723.
+//  Copyright (c) 2015年 masato_arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
